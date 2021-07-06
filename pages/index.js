8c769c22b7d98a3662903df59c4c982c79b4c83e
Mason Aviles <mce.aviles@gmail.com>
2021-07-06 16:15:21 -0700

    take out files, add starting code

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,60 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+    const [question, setQuestion] = useState("Ask me anything...");
+    const [reply, setReply] = useState("Thinking...")
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    function questionAskedHandler(event) {
+        console.log(event);
+        event.preventDefault();
+        const randomReply = Math.random() > .5 ? "Yes" : "No";
+        setQuestion(event.target.question.value);
+        setReply(randomReply)
+    }
+    return (
+        <div>
+            <Head>
+                <title>Magic Eight Ball</title>
+            </Head>
+            <Header />
+            <main className="my-4">
+                <QuestionForm onSubmit={questionAskedHandler} />
+                <EightBall question={question} />
+                <Response reply={reply} />
+            </main>
+            <footer className="px-8 py-6 text-white bg-gray-500">
+                <p>&copy; 2021</p>
+            </footer>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
+
+function EightBall({ question }) {
+    return <div className="relative mx-auto my-8 bg-gray-900 rounded-full h-96 w-96">
+        <div className="absolute flex items-center justify-center w-48 h-48 rounded-full bg-gray-50 top-16 left-16">
+            <p>{question}</p>
+        </div>
+    </div>
+}
+
+function Header() {
+    return <header className="px-8 py-6 bg-gray-500">
+        <h1 className="text-4xl text-white">Magic 8 Ball</h1>
+    </header>
+}
+
+function QuestionForm(props) {
+    return (
+        <form onSubmit={props.onSubmit} className="flex w-1/2 p-2 mx-auto bg-gray-200">
+            <input name="question" className="flex-auto pl-2" />
+            <button className="px-4 py-2 bg-gray-400 text-gray-50">Ask</button>
+        </form>
+    )
+}
+
+function Response({ reply }) {
+    return <p className="text-center">{reply}</p>
+}
+
