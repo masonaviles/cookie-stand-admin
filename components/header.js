87fe5687bf8aea326b8175b7e4b98bcd8187cec6
Mason Aviles <mce.aviles@gmail.com>
2021-07-08 10:43:54 -0700

    add footer and stripes

--- components/header.js
+++ components/header.js
@@ -6,7 +6,7 @@ export default function Header() {
             <header className="p-6 bg-green-500">
                 <h1 className="text-2xl font-semibold">Cookie Stand Admin</h1>
                 <nav>
-                    <Link href="/cookie-stand-admin">
+                    <Link href="/">
                         <a class="mr-4">Admin View</a>
                     </Link>
                     <Link href="/overview">
--- components/report-table.js
+++ components/report-table.js
@@ -7,15 +7,18 @@ export default function ReportTable(props) {
             <table className="w-2/3 py-2 mx-auto my-8 bg-green-200">
                 <thead>
                     <tr>
-                        <th className="border border-green-100">Location</th>
-                        {hours.map(hour => <th className="border border-green-100" key={hour}>{hour}</th>)}
+                        <th className="border border-green-100 odd:bg-green-400">Location</th>
+                        {hours.map(hour => <th className="bg-green-400 border border-green-100" key={hour}>{hour}</th>)}
                     </tr>
                 </thead>
                 <tbody>
                 {props.stand.map((stand) => (
-                    <tr className="pl-2 pr-2 bg-green-200 border border-green-100" key={stand.location}>
-                        <td>{stand.location}</td>
-                        {stand.hourly_sales.map(sales => <td key={sales} className="pl-2 pr-2 bg-green-200 border border-green-100">{sales}</td>)}
+                    <tr className="pl-2 pr-2 bg-green-200 border border-green-100 odd:bg-green-300" key={stand.location}>
+                        <td className="p-1">{stand.location}</td>
+                        {stand.hourly_sales.map(sales => 
+                        <td key={sales} className="pl-2 pr-2 border border-green-100">
+                            {sales}
+                        </td>)}
                     </tr>
                 ))}
                 </tbody>
