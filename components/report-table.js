b8fb2866b51b2ad56a38e31cd8b65498a8d7e284
Mason Aviles <mce.aviles@gmail.com>
2021-07-07 23:21:31 -0700

    change border color on tables

--- components/report-table.js
+++ components/report-table.js
@@ -7,15 +7,15 @@ export default function ReportTable(props) {
             <table className="w-2/3 py-2 mx-auto my-8 bg-green-200">
                 <thead>
                     <tr>
-                        <th className="border border-black">Location</th>
-                        {hours.map(hour => <th className="border border-black" key={hour}>{hour}</th>)}
+                        <th className="border border-green-100">Location</th>
+                        {hours.map(hour => <th className="border border-green-100" key={hour}>{hour}</th>)}
                     </tr>
                 </thead>
                 <tbody>
                 {props.stand.map((stand) => (
-                    <tr className="pl-2 pr-2 bg-green-200 border border-black" key={stand.location}>
+                    <tr className="pl-2 pr-2 bg-green-200 border border-green-100" key={stand.location}>
                         <td>{stand.location}</td>
-                        {stand.hourly_sales.map(sales => <td key={sales} className="pl-2 pr-2 bg-green-200 border border-black">{sales}</td>)}
+                        {stand.hourly_sales.map(sales => <td key={sales} className="pl-2 pr-2 bg-green-200 border border-green-100">{sales}</td>)}
                     </tr>
                 ))}
                 </tbody>
