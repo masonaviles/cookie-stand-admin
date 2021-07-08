d73a9060d43e1b53d3b8c0dfc0d50f030ac4b36e
Mason Aviles <mce.aviles@gmail.com>
2021-07-07 22:16:43 -0700

    map table headers from the data.js

--- components/report-table.js
+++ components/report-table.js
@@ -1,3 +1,5 @@
+import { hours } from '../data/data'
+
 export default function ReportTable() {
 
     return (
@@ -6,6 +8,7 @@ export default function ReportTable() {
             <thead>
                 <tr>
                     <th className="border border-black">Location</th>
+                    {hours.map(item => <th className="border border-black">{item}</th>)}
                 </tr>
             </thead>
             <tbody>
