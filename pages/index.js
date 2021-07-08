e9b2108536cc29599631dd531c00acfd3880ddec
Mason Aviles <mce.aviles@gmail.com>
2021-07-07 19:40:04 -0700

    cookie admin styles and functionality

--- pages/index.js
+++ pages/index.js
@@ -3,58 +3,62 @@ import { useState } from 'react'
 
 export default function Home() {
 
-    const [question, setQuestion] = useState("Ask me anything...");
-    const [reply, setReply] = useState("Thinking...")
+    const [stand, setStand] = useState({ "status": "pending" });
 
-    function questionAskedHandler(event) {
-        console.log(event);
+    function submitHandler(event) {
         event.preventDefault();
-        const randomReply = Math.random() > .5 ? "Yes" : "No";
-        setQuestion(event.target.question.value);
-        setReply(randomReply)
+        const standInfo = {};
+        standInfo.location = event.target.location.value;
+        standInfo.minCustomers = parseInt(event.target.minCustomers.value);
+        standInfo.maxCustomers = parseInt(event.target.maxCustomers.value);
+        standInfo.avgCookies = parseFloat(event.target.avgCookies.value);
+
+        setStand(standInfo);
     }
+
     return (
         <div>
             <Head>
-                <title>Magic Eight Ball</title>
+                <title>Cookie Stand Admin</title>
             </Head>
-            <Header />
-            <main className="my-4">
-                <QuestionForm onSubmit={questionAskedHandler} />
-                <EightBall question={question} />
-                <Response reply={reply} />
+            <header className="p-6 bg-green-500">
+                <h1 className="text-2xl font-semibold">Cookie Stand Admin</h1>
+            </header>
+            <main>
+                <form className="w-2/3 py-2 mx-auto my-8 bg-green-200" onSubmit={submitHandler}>
+                    <fieldset className="w-full p-2">
+                        <legend className="text-xl font-semibold text-center">Create Cookie Stand</legend>
+                        <div className="flex">
+                            <label className="" htmlFor="location">Location</label>
+                            <input className="flex-auto" id="location" name="location" type="text" />
+                        </div>
+                        <div className="flex gap-4">
+
+                            <div className="flex-1">
+                                <label htmlFor="min-customers">Minimum Customers per Hour</label>
+                                <input id="min-customers" name="minCustomers" type="number" required />
+                            </div>
+                            <div className="flex-1">
+                                <label htmlFor="max-customers">Maximum Customers per Hour</label>
+                                <input id="max-customers" name="maxCustomers" type="number" required />
+                            </div>
+                            <div className="flex-1">
+                                <label htmlFor="avg-cookies">Average Cookies per Sale</label>
+                                <input id="avg-cookies" name="avgCookies" type="number" step=".1" required />
+                            </div>
+                            <button className="flex-1 py-2 bg-green-500 rounded">Create</button>
+                        </div>
+                    </fieldset>
+                </form>
+                <pre className="text-center">
+                    <code>
+                        {JSON.stringify(stand)}
+                    </code>
+                </pre>
             </main>
-            <footer className="px-8 py-6 text-white bg-gray-500">
+            <footer className="py-6 pl-6 bg-green-500">
                 <p>&copy; 2021</p>
             </footer>
         </div>
     )
 }
-
-function EightBall({ question }) {
-    return <div className="relative mx-auto my-8 bg-gray-900 rounded-full h-96 w-96">
-        <div className="absolute flex items-center justify-center w-48 h-48 rounded-full bg-gray-50 top-16 left-16">
-            <p>{question}</p>
-        </div>
-    </div>
-}
-
-function Header() {
-    return <header className="px-8 py-6 bg-gray-500">
-        <h1 className="text-4xl text-white">Magic 8 Ball</h1>
-    </header>
-}
-
-function QuestionForm(props) {
-    return (
-        <form onSubmit={props.onSubmit} className="flex w-1/2 p-2 mx-auto bg-gray-200">
-            <input name="question" className="flex-auto pl-2" />
-            <button className="px-4 py-2 bg-gray-400 text-gray-50">Ask</button>
-        </form>
-    )
-}
-
-function Response({ reply }) {
-    return <p className="text-center">{reply}</p>
-}
-
