baa8b3acc5329f3510c9a352083760d9e7a288e0
Mason Aviles <mce.aviles@gmail.com>
2021-07-07 22:18:09 -0700

    fix spacing

--- components/report-table.js
+++ components/report-table.js
@@ -5,16 +5,16 @@ export default function ReportTable() {
     return (
         <>
             <table className="w-2/3 py-2 mx-auto my-8 bg-green-200">
-            <thead>
-                <tr>
-                    <th className="border border-black">Location</th>
-                    {hours.map(item => <th className="border border-black">{item}</th>)}
-                </tr>
-            </thead>
-            <tbody>
-                
-            </tbody>
-        </table>
+                <thead>
+                    <tr>
+                        <th className="border border-black">Location</th>
+                        {hours.map(item => <th className="border border-black">{item}</th>)}
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
         </>
     )
 
